6179e761590e67bfb71fa6d98ab52e0a3cd27ebf
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-06 18:42:35 +0600

    mailcontroller updated

--- app/Http/Controllers/MailVerifyController.php
+++ app/Http/Controllers/MailVerifyController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+
+use App\Mail\OtpMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\ValidationException;
 
 class MailVerifyController extends Controller
 {
-    //
+    public function verifyOtp(Request $request)
+    {
+        try {
+            $attribute = $request->validate([
+                'otp'=>['required'],
+            ]);
+            $user = $request->user();
+            if(!$user)
+            {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+            if($user->otp != $request->otp)
+            {
+                return response()->json(['message' => 'Invalid OTP'], 400);
+            }
+            if (Carbon::now()->isAfter($user->otp_expires_at)) {
+                return response()->json(['message' => 'OTP expired'], 400);
+            }
+            $user->email_verified_at = Carbon::now();
+            $user->otp= null;
+            $user->otp_expires_at=null;
+            $user->save();
+            return response()->json(['message' => 'Email verified successfully']);
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'error' => $e->errors()
+            ]);
+        }
+
+    }
+    public function Resentemail(Request $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return response()->json(['message' => 'Email already verified.']);
+        }
+        $otp = rand(100000, 999999); // email verification otp random generator
+        $user = $request->user();
+        $otp_expires = Carbon::now()->addMinutes(10);
+        $user->otp = $otp;
+        $user->otp_expires_at = $otp_expires;
+        $user->save();
+        Mail::to($user->email)->queue(new OtpMail($otp));
+        return response()->json([
+            'message'=>'email resent',
+        ]);
+    }
+
 }
