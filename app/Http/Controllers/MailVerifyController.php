30e4eaa8218ea6a162a1efcaaf17f3b35491463e
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-18 14:52:06 +0600

    change response of authcontroller

--- app/Http/Controllers/MailVerifyController.php
+++ app/Http/Controllers/MailVerifyController.php
@@ -15,37 +15,36 @@ class MailVerifyController extends Controller
     {
         try {
             $attribute = $request->validate([
-                'otp'=>['required'],
+                'otp' => ['required'],
             ]);
             $user = $request->user();
-            if(!$user)
-            {
+            if (!$user) {
                 return response()->json(['message' => 'User not found'], 404);
             }
-            if($user->otp != $request->otp)
-            {
+            if ($user->otp != $request->otp) {
                 return response()->json(['message' => 'Invalid OTP'], 400);
             }
             if (Carbon::now()->isAfter($user->otp_expires_at)) {
                 return response()->json(['message' => 'OTP expired'], 400);
             }
             $user->email_verified_at = Carbon::now();
-            $user->otp= null;
-            $user->otp_expires_at=null;
+            $user->otp = null;
+            $user->otp_expires_at = null;
             $user->save();
-            return response()->json(['message' => 'Email verified successfully']);
-
+            return response()->json([
+                'message' => 'Email verified successfully',
+                'user' => $user,
+            ]);
         } catch (ValidationException $e) {
             return response()->json([
                 'error' => $e->errors()
-            ],422);
+            ], 422);
         }
-
     }
     public function Resentemail(Request $request)
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return response()->json(['message' => 'Email already verified.'],409);
+            return response()->json(['message' => 'Email already verified.'], 409);
         }
         $otp = rand(100000, 999999); // email verification otp random generator
         $user = $request->user();
@@ -55,8 +54,7 @@ class MailVerifyController extends Controller
         $user->save();
         Mail::to($user->email)->queue(new OtpMail($otp));
         return response()->json([
-            'message'=>'email resent',
+            'message' => 'email resent',
         ]);
     }
-
 }
